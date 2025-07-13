ec8ff0de340c864f21d136b006710a118b7d37b6
jubayer718 <jubayerahmed2462@gmail.com>
2025-07-13 07:39:14 +0600

    major bug fixed.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 "use client";
 
 import { signIn } from "next-auth/react";
 import { usePathname } from "next/navigation";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {FaGoogle} from "react-icons/fa6"
 
 
 export default function LoginPage() {
@@ -14,18 +18,41 @@ export default function LoginPage() {
       callbackUrl: currentPath || "/",
     })
   }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const fromData = new FormData(form);
+
+    const name = fromData.get("name") as string;
+    const email = fromData.get("email") as string;
+
+    const userInfo = {
+      name: name,
+      email: email,
+      role: 'user'
+    }
+
+
+  }
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-4">
-      <div className="bg-white rounded-xl shadow-xl p-8 w-full max-w-md text-center">
+      <div className="bg-amber-100 rounded-xl shadow-xl p-8 w-full max-w-md text-center">
         <h1 className="text-2xl font-bold mb-4">Login to Your Account</h1>
         <p className="text-gray-600 mb-6">Use your Google account to continue</p>
-
-        <button
+        <form className="my-6 space-y-4" onSubmit={handleSubmit}>
+          
+          <Input className="placeholder:text-sm font-semibold" type="text" name="name" id="name" placeholder="Name" required />
+          
+          <Input className="placeholder:text-sm font-semibold" type="email" name="email" id="email" placeholder="Email" required />
+          <Button type="submit">Submit</Button>
+        </form>
+        <Button
           onClick={handleLogin}
-          className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-2 rounded-lg"
+          className=" text-white px-6 py-2 rounded-lg"
         >
-          Continue with Google
-        </button>
+          <FaGoogle size={24}/>Continue with Google
+        </Button>
       </div>
     </div>
   );
