58d16a9d3bc4eee8a787315d74f883426e83c962
jubayer718 <jubayerahmed2462@gmail.com>
2025-07-13 12:04:52 +0600

    deploy test.

--- src/app/college-details/[id]/page.tsx
+++ src/app/college-details/[id]/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+// import { notFound } from "next/navigation";
 
-const DetailsPage = () => {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+const DetailsPage = ({ params }: PageProps) => {
+  // const { id } = params;
   return (
-    <div className='my-14'>
-      <h1>details</h1>
+    <div className='my-16'>
+      <h1>Details Page for ID: {params.id}</h1>
     </div>
   );
 };
